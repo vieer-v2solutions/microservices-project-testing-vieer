6833a8c9cb568a0b3352e2e435f09f7680d3d584
Suvam21 <suvambehera21@gmail.com>
2023-08-21 15:35:40 +0530

    initial commit

---
+++ ratingMicroservice/ratingMicroservice/src/test/java/com/mpv2/ratingMicroservice/RatingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpv2.ratingMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
