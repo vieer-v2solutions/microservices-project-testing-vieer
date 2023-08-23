f48293a3f1c2c304a15c7ae69de05d6420756279
Suvam21 <suvambehera21@gmail.com>
2023-08-23 22:07:21 +0530

    Revert "Test Changes"
    This reverts commit e74c7eaedef82301bbe5cd61d1130a7cf4b14e75.

--- hotelMicroservice/hotelMicroservice/src/main/java/com/mpv2/hotelMicroservice/HotelMicroserviceApplication.java
+++ hotelMicroservice/hotelMicroservice/src/main/java/com/mpv2/hotelMicroservice/HotelMicroserviceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelMicroserviceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HotelMicroserviceApplication.class, args);
 	}
 
