18805f539051c1498c7d679e33465d0728ac1f15
Suvam21 <suvambehera21@gmail.com>
2023-08-24 17:59:05 +0530

    resttemplate implementation

---
+++ userMicroservice/userMicroservice/src/main/java/com/mpv2/userMicroservice/config/UserConfiguration.java
@@ -0,0 +1,14 @@
+package com.mpv2.userMicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class UserConfiguration {
+    
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
--- userMicroservice/userMicroservice/src/main/java/com/mpv2/userMicroservice/service/UserServiceImplementation.java
+++ userMicroservice/userMicroservice/src/main/java/com/mpv2/userMicroservice/service/UserServiceImplementation.java
@@ -1,16 +1,27 @@
 package com.mpv2.userMicroservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.mpv2.userMicroservice.entity.Ratings;
 import com.mpv2.userMicroservice.entity.User;
 import com.mpv2.userMicroservice.exception.ResourceNotFoundException;
 import com.mpv2.userMicroservice.repository.UserRepository;
 
+
 @Service
 public class UserServiceImplementation implements UserService {
+
+    public Logger logger = LoggerFactory.getLogger(UserServiceImplementation.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private UserRepository userRepository;
@@ -27,7 +38,13 @@ public class UserServiceImplementation implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User Not found on server with id: " + userId));
+        User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User Not found on server with id: " + userId));
+
+        ArrayList<Ratings> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId() , ArrayList.class);
+        logger.info("{}", ratingsOfUser);
+
+        user.setRatings(ratingsOfUser);
+        return user;
     }
 }
 
