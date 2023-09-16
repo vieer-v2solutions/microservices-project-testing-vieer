23d849f4017fe32103b7b63109985e6c33b2d219
Your Name <you@example.com>
2023-09-16 11:27:39 +0530

    new file:   Dockerfile 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/mpv2/serviceRegistry/ServiceRegistryApplication.java 	new file:   src/main/java/com/mpv2/serviceRegistry/config/CorsConfig.java 	new file:   src/main/resources/application.properties 	new file:   src/main/resources/application.yml 	new file:   src/test/java/com/mpv2/serviceRegistry/ServiceRegistryApplicationTests.java

---
+++ src/main/java/com/mpv2/serviceRegistry/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.mpv2.serviceRegistry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("http://frontend-vue-app.s3-website.ap-south-1.amazonaws.com"); // apna ip-address:3000 dalna, localhost pe chalta nahi hai kabhi kabar
+		corsConfig.addAllowedHeader("*");
+		corsConfig.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsFilter(source);
+	}
+}
+
